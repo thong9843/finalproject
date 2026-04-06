ba2839c3b0b9932c247f6138aa78d58137f42c0e
Le Duy Thong <leduythong1717@gmail.com>
2026-04-06 18:54:57 +0700

    add new function

--- backend/src/routes/statRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const statController = require('../controllers/statController');
-const { verifyToken } = require('../middlewares/auth');
-
-router.use(verifyToken);
-router.get('/dashboard', statController.getDashboardStats);
-
-module.exports = router;
